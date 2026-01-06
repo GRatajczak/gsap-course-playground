92edb8211fac55742e791a0cce5dd492a7a6d8b9
Grzegorz Ratajczak <g.ratajczak97@gmail.com>
2026-01-06 17:19:15 +0100

    add first animation

---
+++ pages/01a_your-first-animation/script.js
@@ -0,0 +1,49 @@
+gsap.to(".card", {
+    scale: 1,
+    opacity: 1,
+    duration: 2,
+    onComplete: () => {
+        gsap.to(".card", {
+            y: -20,
+            duration: 0.2,
+            onComplete: () => {
+                gsap.to(".card", {
+                    x: 100,
+                    rotate: 90,
+                    duration: 1,
+                    onComplete: () => {
+                        gsap.to(".card", {
+                            x: -100,
+                            rotate: -90,
+                            duration: 1,
+                            onComplete: () => {
+                                gsap.to(".card", {
+                                    x: 0,
+                                    rotate: 0,
+                                    duration: 1,
+                                    onComplete: () => {
+                                        gsap.to(".card", {
+                                            x: 0,
+                                            y: -20,
+                                            duration: 1,
+                                            scale: 0.8,
+                                            onComplete: () => {
+                                                gsap.to(".card", {
+                                                    boxShadow:
+                                                        "0 20px 80px rgba(29, 209, 161, 0.6)",
+                                                    repeat: -1,
+                                                    yoyo: true,
+                                                    duration: 1,
+                                                });
+                                            },
+                                        });
+                                    },
+                                });
+                            },
+                        });
+                    },
+                });
+            },
+        });
+    },
+});
